6ad42e4f64ac69225e7ae7e3bb49bca4f8f49224
Ryan Chhong <ryanchhong@gmail.com>
2019-10-15 03:43:09 -0500

    More layout stuff

--- App.js
+++ App.js
@@ -19,13 +19,12 @@ export default class App extends Component
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={{flex : 1, backgroundColor : "#FF9999"}}>
-          <View style={{flex: 5}}>
+          <View style={{flex: 4}}>
             <ScrollView
               contentInsetAdjustmentBehavior="automatic"
               style={{backgroundColor : "#998833"}}>
             </ScrollView>
           </View> 
-
           <View style={{flex: 1, backgroundColor : "#00FF00"}}>
             <Info />
           </View>
--- views/Info.js
+++ views/Info.js
@@ -3,26 +3,36 @@ import {
     View,
     Text,
     StyleSheet,
+    Button
 } from 'react-native'
 
 const styles = StyleSheet.create({
-    scrollView: {
-      backgroundColor: "#FF0000",
-      flexGrow: 5
-    },
-    body: {
-      backgroundColor: "#FFFFFF",
-    },
 });
 
 export default class Info extends Component {
     render() {
         return (
-            <View style={{flex: 1}}>
-                <Text>aa</Text>
-                <Text>aa</Text>
-                <Text>aa</Text>
+            <View style={{ flex: 1, flexDirection: "column" }}>
+                <View style={{ flex: 1, flexDirection: "row",  marginLeft: '5%', marginTop: "5%"}}>
+                    <View style={{ width: 65, height: 65, backgroundColor: "#FFFFFF" }} />
+                    <View style={{ flexDirection: "column", marginLeft: 10 }}>
+                        <Text style={{ fontSize: 30 }}>aaaaaaa</Text>
+                        <Text style={{ fontSize: 15 }}>aaa</Text>
+                    </View>
+                </View>
+                <View style={{flex: 1, marginTop: '5%', marginLeft: "3%", flexDirection: 'row', width: "50%", justifyContent: "space-around"}}>
+                    <View>
+                    <Button title="Prev"/>
+                    </View>
+                    <View>
+                    <Button title="Pause"/>
+                    </View>
+                    <View>
+                    <Button title="Next"/>
+                    </View>
+                </View>
             </View>
+
         );
     }
 }
